f3a1233a4141f39604ac403eacfda6c71fb2a649
Mikhail <mikhailtwere@gmail.com>
2022-07-07 22:24:51 -0600

    commit tree 17ad81426480e550199ae9399ac427af0ca57594

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(function (response){
+    return response.json();
+  })
+  .then(function (data){
+    renderBooks(data)
+    console.log(data);
+  })
 }
 
 function renderBooks(books) {
